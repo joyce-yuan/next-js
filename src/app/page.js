2fe19602db5e7650da7ec730a2f03bdb3b17cd3f
Joyce-Yuan <jyuan24@mit.edu>
2023-11-05 18:54:51 -0500

    button

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import {
-  Button,
-} from 'react';
+import Button from 'react';
 
 export default function Home() {
   return (
